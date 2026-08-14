658f502614e66e27d6a5e298dfe622f87b5d314d
WAHIDI1970 <mouad066989gmail.com>
2026-08-14 05:30:14 +0200

    chore: add package-lock.json for GitHub Actions

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,13 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/Portfolio-iotous/',
+  base: '/Portfolio-io/',
   plugins: [
     react(),
     tailwindcss(), // <-- Ajout du plugin Tailwind v4
   ],
   build: {
-    outDir: 'dist',
+    outDir: 'docs',
     rollupOptions: {
       output: {
         manualChunks: undefined,
